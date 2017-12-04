d7c52d155d16e2be834fed7b7e73e0cfc754ea62
Aylon Spigel <aylon.spigel@gmail.com>
2017-12-04 06:50:18 +0000

    portfolio plus

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,14 @@ module DevcampPortfolio
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # code to influence the scaffold generator.
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework  :test_unit, fixture: false
+      g.stylesheets      false
+      g.javascript       false
+    end
 
     # Don't generate system test files.
     config.generators.system_tests = nil
